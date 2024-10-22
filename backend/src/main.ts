0e879bfea2f9d073e09b43da33f57477f31de7c0
noaGitlin <noa@montopay.com>
2024-10-22 13:16:44 +0300

    update port fowarding7

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
 // Define allowed origins
 const allowedOrigins = [
   'http://localhost:5173',
-  'https://first-app-*.vercel.app'
+  /https:\/\/.*\.vercel\.app/
 ];
 
 // Enable CORS
