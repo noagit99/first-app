f9568543d8af3fdf91c91bd34d82b4a046702003
noaGitlin <noa@montopay.com>
2024-10-22 16:51:29 +0300

    plzwork71

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
+import { resolve } from 'path';
+
 
 export default defineConfig({
   plugins: [react(), vanillaExtractPlugin()],
@@ -8,5 +10,13 @@ export default defineConfig({
     'process.env': {
       API_URL: process.env.VITE_API_URL || 'https://njjbwcjd-3000.euw.devtunnels.ms', // Fallback to your port-forwarding link
     },
-  },
+    build: {
+      sourcemap: true,
+      outDir: resolve(__dirname, 'dist'),
+      target: 'esnext',
+      emptyOutDir: true,
+      rollupOptions: {
+      }
+    }
+  }
 });
